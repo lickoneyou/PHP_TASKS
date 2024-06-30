92295d59d247805b550cc6fa91f22c36f6d40864
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-06-30 13:57:31 +0300

    added about.php

---
+++ components/about.php
@@ -0,0 +1,15 @@
+<section class='about'>
+  <div class='aboutImg'></div>
+  <div>
+    <div>
+      <h2 class='title'>The Backstage of the Wilderness World.</h2>
+      <p class='titleDescr'>The site was founded on the basis of a volunteer movement to protect and care for animals.</p>
+    </div>
+
+    <div>
+      <h3 class='subTitle'>How it works </h3>
+      <p class='subTitleDescr'>The main goal is to help the animals, as well as the nature reserves and zoos where they are kept. We are currently working on video projects targeting pandas in China, eagles on an island near Los Angeles, alligators in Florida and gorillas in the Congo. These have a total of more than 1,500 mammals and reptiles.</p>
+    </div>
+  </div>
+  <img class='aboutFlower' src='../assets/img/aboutflower.png' alt='flower'/>
+</section>
