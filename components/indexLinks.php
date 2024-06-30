7e61dde1d62beecbc09aada3dd2dc2078176ee27
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-06-30 16:51:31 +0300

    added testimonials.css link

--- components/indexLinks.php
+++ components/indexLinks.php
@@ -1,3 +1,4 @@
 <link rel='stylesheet'  href='../assets/css/about.css'>
 <link rel='stylesheet'  href='../assets/css/zoos.css'>
 <link rel='stylesheet'  href='../assets/css/map.css'>
+<link rel='stylesheet'  href='../assets/css/testimonials.css'>
