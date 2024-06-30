9f70b46006b0170e2063f21bb224a29261009e16
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-06-30 16:51:44 +0300

    added testimonials.php

---
+++ components/testimonials.php
@@ -0,0 +1,104 @@
+<?php
+
+function createTestimonialCard()
+{
+    $peopleArr = [
+        [
+            'name' => 'Michael John',
+            'avatar' => '../assets/img/avatars/1.jpg',
+            'contry' => 'Austria',
+            'visited' => 'Today',
+            'testimonial' => 'The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.
+The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.
+',
+        ],
+        [
+            'name' => 'Oskar Samborsky',
+            'avatar' => '../assets/img/avatars/2.jpg',
+            'contry' => 'Austria',
+            'visited' => 'Yesterday',
+            'testimonial' => 'Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. 
+The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.
+
+The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.
+',
+        ],
+        [
+            'name' => 'Fredericka Michelin',
+            'avatar' => '../assets/img/avatars/3.jpg',
+            'contry' => 'Austria',
+            'visited' => 'Yesterday',
+            'testimonial' => 'The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.
+The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met.
+The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.
+',
+        ],
+        [
+            'name' => 'Mila Riksha',
+            'avatar' => '../assets/img/avatars/4.jpg',
+            'contry' => 'Austria',
+            'visited' => 'Yesterday',
+            'testimonial' => 'My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas.
+The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.
+',
+        ],
+        [
+            'name' => 'Betty R. Turbeville',
+            'avatar' => '../assets/img/avatars/5.jpg',
+            'contry' => 'Austria',
+            'visited' => 'Yesterday',
+            'testimonial' => 'My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas.
+The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.
+',
+        ],
+        [
+            'name' => 'Emily B. Conway',
+            'avatar' => '../assets/img/avatars/6.jpg',
+            'contry' => 'Austria',
+            'visited' => 'Yesterday',
+            'testimonial' => 'My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas.
+The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.
+',
+        ],
+        [
+            'name' => 'Francis J. Anderson',
+            'avatar' => '../assets/img/avatars/7.jpg',
+            'contry' => 'Austria',
+            'visited' => 'Yesterday',
+            'testimonial' => 'My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas.
+The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.
+',
+        ],
+        [
+            'name' => 'Richard M. Simpson',
+            'avatar' => '../assets/img/avatars/8.jpg',
+            'contry' => 'Austria',
+            'visited' => 'Yesterday',
+            'testimonial' => 'My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas.
+The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.
+',
+        ],
+    ];
+
+    foreach ($peopleArr as $key) {
+        echo "<div class='testimonialCard'>
+      <div class='testimonialCardAboutMan'>
+        <img src='$key[avatar]' alt='$key[name]’s avatar'>
+        <div>
+        <h3>$key[name]</h3>
+        <p>Local $key[contry] * $key[visited]</p>
+        </div>
+      </div>
+      <p class='testimonial'>$key[testimonial]</p>
+    </div>";
+    }
+} ?>
+
+<section class='testimonials'>
+  <h2>Testimonials</h2>
+  <div class='testimonialsCardContainer'>
+    <?php createTestimonialCard(); ?>
+  </div>
+  <button>leave feedback</button>
+  <img src='../assets/img/leavtestimonial.png' alt='leav'>
+</section>
